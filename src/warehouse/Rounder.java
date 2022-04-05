ae635f0264c7260a7c6f06f3889d6e6535cdffaf
Belialv <amuisher79@gmail.com>
2022-04-05 17:25:57 +0300

    commit

---
+++ src/warehouse/Rounder.java
@@ -0,0 +1,14 @@
+package warehouse;
+
+import java.text.DecimalFormat;
+
+public class Rounder {
+
+    // Метод округления веса
+    public static String roundWeight(double value) {
+        // Шаблон для веса, до 3-х знаков после
+        // десятичного разделителя
+        DecimalFormat df = new DecimalFormat("0.000");
+        return df.format(value);
+    }
+}
